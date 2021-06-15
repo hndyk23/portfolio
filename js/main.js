b112610c51c713636754f09c44227b6b9ef8b7b3
hndyk23 <85877024+hndyk23@users.noreply.github.com>
2021-06-15 22:07:00 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(function(){
+      $('.works-modal1').click(function(){
+        $('#pix-tech').fadeIn("slow");
+      });
+
+      $('#pix-tech').click(function(){
+        $('#pix-tech').fadeOut("slow");
+      });
+　　});
+
+$(function(){
+      $('.works-modal2').click(function(){
+        $('#brige').fadeIn("slow");
+      });
+
+      $('#brige').click(function(){
+        $('#brige').fadeOut("slow");
+      });
+    });
+
+$(function(){
+      $('.works-modal3').click(function(){
+        $('#bxslider').fadeIn("slow");
+      });
+
+      $('#bxslider').click(function(){
+        $('#bxslider').fadeOut("slow");
+      });
+    });
+
+$(function(){
+      $('.works-modal4').click(function(){
+        $('#lightbox').fadeIn("slow");
+      });
+
+      $('#lightbox').click(function(){
+        $('#lightbox').fadeOut("slow");
+      });
+    });
+
+$(function(){
+      $('.works-modal5').click(function(){
+        $('#raindrop').fadeIn("slow");
+      });
+
+      $('#raindrop').click(function(){
+        $('#raindrop').fadeOut("slow");
+      });
+    });
